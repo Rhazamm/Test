49bbcb7029950dab819ea32104a2319a05039953
rhazam <Nevfel.ates@web.de>
2023-10-05 02:00:50 +0200

    jo

---
+++ 3.5.js
@@ -0,0 +1,10 @@
+"use strict"
+//mal
+console.log ("5*4:", 5*4)
+//modulo
+console.log(11 % 6)
+// poitenz
+console.log(Math.max(1,5,10))
+console.log()
+console.log()
+console.log()    
---
+++ 3.6.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let s = "2.534"
+
+//string zu ganzzahl
+console.log(parseInt(s,10))
+//string zu kommazahl 
+console.log(parseFloat(s,))
+
+//achtung: kein plan no kommazahlen verwenden
+
+//zahl zu string
+let n = 2.789
+console.log(""+ n)
